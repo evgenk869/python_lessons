c27f1038adefc032e12b2648fd253b612fe0493d
wisp_mac <evgenk869@gmail.com>
2024-12-23 20:44:13 +0200

    add homework #2 task

--- main.py
+++ main.py
@@ -51,11 +51,46 @@
 # elif 12 > hours >= 0:
 #     print('AM')
 # else: print('Incorrect time!')
+#
+# """new_task"""
+# ratio = int(input('Enter film ratio: '))
+# if 0 < ratio <= 5:
+#     if ratio == 4 or ratio == 5:
+#         print('Good film')
+#     else:
+#         print('Bad film!')
 
-"""new_task"""
-ratio = int(input('Enter film ratio: '))
-if 0 < ratio <= 5:
-    if ratio == 4 or ratio == 5:
-        print('Good film')
-    else:
-        print('Bad film!')
+# """homework #2"""
+#
+# number1 = int(input('Enter first number: '))
+# number2 = int(input('Enter second number: '))
+# number3 = int(input('Enter third number: '))
+#
+#
+# if number2 < number1 > number3:
+#     print('The biggest number is:', number1)
+# elif number1 < number2 > number3:
+#     print('The biggest number is:', number2)
+# elif number1 < number3 > number2:
+#     print('The biggest number is:', number3)
+#
+# if number2 > number1 < number3:
+#     print('The minimum number is:', number1)
+# elif number1 > number2 < number3:
+#     print('The minimum number is:', number2)
+# elif number1 > number3 < number2:
+#     print('The minimum number is:', number3)
+#
+# print('Middel of sum is:', ((number1 + number2 + number3)/3))
+
+meters = int(input('Enter amoun of meters: '))
+coefficient = (input('Chose type of value (miles, yards, inch): '))
+
+if coefficient == 'miles':
+    print('Your meters = ', meters * 0.000621371,'miles')
+elif coefficient == 'yards':
+    print('Your meters = ', meters * 1.09361,'yards')
+elif coefficient == 'inch':
+    print('Your meters = ', meters * 39.3701, 'inches')
+else:
+    print('Invalid input. Please choose "miles", "yards", or "inch".')
